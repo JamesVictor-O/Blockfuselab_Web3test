2b0478151ea6c9ecebc8d247adfd5c6542b93669
JamesVictor-o <victorjames40v@gmail.com>
2024-12-02 11:28:06 +0100

    final commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // write a function to find the longest prefix string amount in an array of strings
 // if there is no common prefix return empty string
-let input=["flower", "flight"]
+let input=["flower", "flight","flow"]
 function commonLongestPrefix(arr){
-    let [a,b]=arr
+    let [a,b,c]=arr
     let commonPrefix=""
     let lengthOfA=a.length;
     let lengthOfb=b.length;
+    let lengthOfc=c.length;
 
-    let minLengthOfAB=Math.min(lengthOfA,lengthOfb)
+    let minLengthOfAB=Math.min(lengthOfA,lengthOfb,lengthOfc)
 
     for(i=0; i< minLengthOfAB; i++){
-        if(a[i]=== b[i]){
+        if(a[i]=== b[i] && a[i]=== c[i]){
             commonPrefix += a[i]
         }else{ 
            break
