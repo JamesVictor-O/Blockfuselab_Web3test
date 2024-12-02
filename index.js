61bea6b05347cf214a520c4dace2a0cf23437c0b
JamesVictor-o <victorjames40v@gmail.com>
2024-12-02 10:07:41 +0100

    wrote the function to return the prefixed values

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write a function to find the longest prefix string amount in an array of strings
+// if there is no common prefix return empty string
+let input=["flower", "dog"]
+function commonLongestPrefix(arr){
+    let [a,b]=arr
+    let commonPrefix=""
+    let lengthOfA=a.length;
+    let lengthOfb=b.length;
+
+    let minLengthOfAB=Math.min(lengthOfA,lengthOfb)
+    for(i=0; i< minLengthOfAB; i++){
+        if(a[i]=== b[i]){
+            commonPrefix += a[i]
+        }else{
+            return ""
+        }
+    }
+    return commonPrefix
+}
+
+
+console.log(commonLongestPrefix(input))
